e7c22ecf6cfe49da3058055fcec9306f88cf4d1e
leonardo.walkowski <leowalkowski@gmail.com>
2024-07-02 16:19:10 -0300

    react-axios-parte-3

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import blogFetch from "../axios/config";
+
+import {useState} from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import "./NewPost.css";
 
 const NewPost = () => {
+  const navigate = useNavigate()
+
+  const [ title, setTitle] = useState()
+  const[body, setBody] = useState()
+
   return (
-    <div>
-      <p>NewPost</p>
+    <div className="new-post">
+      <h2>Inserir novo Post:</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input type="text" name="title" id="title" placeholder="Digite o título"/>
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea name="title" id="title" placeholder="Digite o conteúdo"></textarea>
+        </div>
+        <input type="submit" value="Criar Post" className="btn" />
+      </form>
     </div>
   )
-}
+};
 
 export default NewPost
