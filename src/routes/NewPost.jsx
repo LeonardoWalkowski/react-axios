04c99d47adf1410f6b2d648a2bb6c7175b3155d9
leonardo.walkowski <leowalkowski@gmail.com>
2024-07-03 11:13:29 -0300

    react-axios-parte-final

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,6 +1,6 @@
 import blogFetch from "../axios/config";
 
-import {useState} from "react";
+import { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 
@@ -9,13 +9,19 @@ import "./NewPost.css";
 const NewPost = () => {
   const navigate = useNavigate();
 
-  const[title, setTitle] = useState();
-  const[body, setBody] = useState();
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
 
-  const createPost = (e) => {
+  const createPost = async (e) => {
     e.preventDefault();
 
-    console.log(title, body);
+    const post = { title, body, userId: 1};
+
+    await blogFetch.post("/posts", {
+      body: post,
+    });
+
+    navigate("/");
   };
 
   return (
@@ -24,21 +30,21 @@ const NewPost = () => {
       <form onSubmit={(e) => createPost(e)}>
         <div className="form-control">
           <label htmlFor="title">Título:</label>
-          <input 
-          type="text" 
-          name="title" 
-          id="title" 
-          placeholder="Digite o título"
-          onChange={(e) => setTitle(e.target.value)}
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Digite o título"
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="form-control">
           <label htmlFor="body">Conteúdo:</label>
-          <textarea 
-          name="body" 
-          id="body" 
-          placeholder="Digite o conteúdo"
-          onChange={(e) => setBody(e.target.value)}
+          <textarea
+            name="body"
+            id="body"
+            placeholder="Digite o conteúdo"
+            onChange={(e) => setBody(e.target.value)}
           ></textarea>
         </div>
         <input type="submit" value="Criar Post" className="btn" />
